c7dd01ded357af6bde33819129f52e4b2ed731d3
junior <junior@MacBook-Pro-de-junior.local>
2026-04-10 10:16:38 +0900

    update

--- app/(tabs)/read/_layout.tsx
+++ app/(tabs)/read/_layout.tsx
@@ -2,10 +2,20 @@ import { Stack } from 'expo-router';
 
 export default function ReadLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        name="[book]/index"
+        options={{
+          headerShown: true,
+        }}
+      />
+    </Stack>
   );
 }
